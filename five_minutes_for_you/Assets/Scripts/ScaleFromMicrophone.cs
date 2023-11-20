a8c5c87356cad30baa50c917f7ce1ed58477267c
Hafsa Khallouk <hafsa-k@hotmail.com>
2023-11-20 14:28:53 +0100

    creation button go back pour le pissenlit

--- five_minutes_for_you/Assets/Scripts/ScaleFromMicrophone.cs
+++ five_minutes_for_you/Assets/Scripts/ScaleFromMicrophone.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
 
 public class ScaleFromMicrophone : MonoBehaviour
 {
@@ -9,13 +11,20 @@ public class ScaleFromMicrophone : MonoBehaviour
     public Vector3 minScale; 
     public Vector3 maxScale; 
     public audioLoudness detector;
-
+    public int compteur = 0;
+    public GameObject pissenlit;
     public float loudnessSensibility = 100; 
     public float threshold = 0.1f;
+
+    public GameObject messageUI; 
+    public Button boutonRetour;
 
     // Start is called before the first frame update
     void Start()
     {
+        messageUI.SetActive(false); // Désactiver le message au démarrage du jeu
+        
+        boutonRetour.gameObject.SetActive(false);
         
     }
 
@@ -26,9 +35,27 @@ public class ScaleFromMicrophone : MonoBehaviour
         if (loudness >= threshold) {
             // Si le son est supérieur ou égal au seuil, activer l'animation
             animator.SetBool("AudioOn", true);
+            compteur+=1;
         } else {
             // Sinon, désactiver l'animation
             animator.SetBool("AudioOn", false);
         }
+
+        if (compteur >= 1500) AfficherMessage();;
+    }
+
+     void AfficherMessage()
+    {
+        pissenlit.SetActive(false);
+        // Affichez ici le message pour indiquer que le seuil a été atteint
+        messageUI.SetActive(true);
+
+        boutonRetour.gameObject.SetActive(true);
+        boutonRetour.onClick.AddListener(RetourScenePrecedente); 
+    }
+    void RetourScenePrecedente()
+    {
+        // Chargez la scène précédente
+        SceneManager.LoadScene("game 0");
     }
 }
